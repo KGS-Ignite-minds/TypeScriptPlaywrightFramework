a781d0b456684670877d1c8fe75f32bc87e101b8
amit Deshpande <amitdeshpande1@kpmg.com>
2024-01-04 13:22:53 +0530

    Clean up - removed BidGPT specific artefacts

--- POM/loginPage.ts
+++ POM/loginPage.ts
@@ -1,6 +1,5 @@
 import { expect, type Locator, type Page } from '@playwright/test';
 import { page } from '../hooks/hooks'
-import * as TestLocators from '../POM/TestLocators.json'
 import CommonPage from '../POM/common'
 import * as TestData from '../TestData/TestData.json'
 
@@ -10,56 +9,10 @@ export default class LoginPage {
     constructor(page: Page) {
         this.page = page;
     }
-    async navigateToBidGPTSignIn() {
-        let common = new CommonPage(this.page);
-        await common.launchApplication()
-        
-        await common.clickObject(TestLocators.Login.IAgree, "I agree")
-        await common.clickObject(TestLocators.Login.Continue, "Continue")
-        await common.waitForElement(TestLocators.Login.SignInWithMicrosoft)
-    }
-
-    async loginToBidGPT(UserName: string, Password: string) {
-        let common = new CommonPage(this.page);
-        await common.launchApplication()
-        
-        await common.validateVisibleText("Reminder Text",TestData.TestCaseData.TC001_Login.ReminderText1)
-        await common.validateVisibleText("Reminder Text",TestData.TestCaseData.TC001_Login.ReminderText2)
-        await common.clickObject(TestLocators.Login.KPMGTermsOfService,"KPMGs terms of service.")
-        await common.clickObject(TestLocators.Login.Close,"Close")
-        await common.clickObject(TestLocators.Login.KPMGTermsOfService,"KPMGs terms of service.")
-        await common.clickObject(TestLocators.Login.CloseIcon,"Close")
-        await common.clickObject(TestLocators.Login.IAgree, "I agree")
-        await common.clickObject(TestLocators.Login.Continue, "Continue")
-        await common.clickObject(TestLocators.Login.SignInWithMicrosoft, "Sign in with microsoft")
-        await common.enterText(TestLocators.Login.UserName,"User Name", UserName)
-        await common.clickObject(TestLocators.Login.Next, "Next")
-        await common.enterText(TestLocators.Login.Password,"User Name", Password)
-        await common.clickObject(TestLocators.Login.SignIn, "Sign in")
-        await common.clickObject(TestLocators.Login.NextOnMoreInfoRequired, "Next on More Information Required screen")
-        await common.clickObject(TestLocators.Login.SkipSetUp, "Skip setup")
-        await common.clickObject(TestLocators.Login.Yes, "Yes on Stay signed in? screen screen")
-        await common.waitForElement(TestLocators.Login.NewQuestion)
-    }
-
-    async validateKPMGTermsOfService(UserName: string, Password: string) {
-        let common = new CommonPage(this.page);
-        await common.validateVisibleText("KPMG Terms Of Service",TestData.TestCaseData.TC001_Login.ReminderText1)
-
-    }
-
-    async verifyBackEndURL() {
-        let common = new CommonPage(this.page);
-        await common.launchBackEndApplication()
-
-        await common.validateElementVisible(TestLocators.Login.BackEndMessage,"BackEndMessage")
-    }
 
     async launchApplicationUrl() {
         let common = new CommonPage(this.page);
         await common.launchApplicationUrl()
-        
-
     }
 
 
--- tests/steps/Login.steps.ts
+++ tests/steps/Login.steps.ts
@@ -2,38 +2,20 @@ import { Given, When, Then } from "@cucumber/cucumber"
 import { page } from '../../hooks/hooks'
 import LoginPage from '../../POM/loginPage'
 import Common from '../../POM/common'
-import * as TestLocators from '../../POM/TestLocators.json'
 
-Given('I navigate to BidGPT Sign In page', async () => {
+Given('I Navigate to the AUT', async () => {
      const login = new LoginPage(page);
-    await login.navigateToBidGPTSignIn()
-})
-
-Given('I enter {string} and {string} and Sign In to the BidGPT application', async (UserName:string, Password: string) => {
-     const login = new LoginPage(page);
-    await login.loginToBidGPT(UserName,Password)
-})
-
-Given('I launch BidGPT backend application', async () => {
-     const login = new LoginPage(page);
-    await login.verifyBackEndURL()
-})
-
-
-Given('I launch the application url', async () => {
-    const login = new LoginPage(page);
     await login.launchApplicationUrl()
 })
 
 Then('I click on dropdown option', async () => {
-    const common = new Common(page);
-    await common.clickObject(TestLocators.HomePage.dropdown,'Dropdown')
+    
   })
 
   
 Then("I select a value {string} from the dropdown", async (str) =>{
-    const common = new Common(page);
-    common.selectDropdownByValue(TestLocators.HomePage.dropdownObject,str)
+    
+    
 
 })
 
